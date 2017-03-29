390711ad6f429db7c60e9c657f24b6627e481d68
cat9563 <crystal9563@gmail.com>
2017-03-29 17:59:38 +0000

    updated models

---
+++ db/migrate/20170329175012_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text  :description
+      t.integer :pages
+      t.string  :author_id
+      t.string  :amazon_url
+      t.string  :picture_url
+
+      t.timestamps
+    end
+  end
+end
